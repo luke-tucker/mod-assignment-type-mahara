038dc7394fbf3c7d783ac80e7aeee1451ce1ae62
Dominique-Alain Jan <djan@mac.com>
2010-02-17 15:38:22 +1300

    French translation
    Signed-off-by: Richard Mansfield <richardm@catalyst.net.nz>

---
+++ lang/fr_utf8/assignment_mahara.php
@@ -0,0 +1,15 @@
+<?PHP // $Id$ 
+      // 
+
+$string['clicktopreview'] = 'cliquez pour un aperçu en pleine taille';
+$string['clicktoselect'] = 'cliquez pour sélectionez l\'exposition';
+$string['noviewsfound'] = 'Vous n\'avez créé aucune exposition dans $a';
+$string['preview'] = 'Aperçu';
+$string['selectedview'] = 'Exposition envoyée pour évaluation';
+$string['selectmaharaview'] = 'Sélectionnez une exposition du portfolio $a->name  dans cette liste, ou <a href=\"$a->jumpurl\">cliquez ici</a> pour visiter $a->name et créer une exposition maintenant.';
+$string['title'] = 'Titre';
+$string['typemahara'] = 'Portfolio Mahara';
+$string['views'] = 'Expositions';
+$string['viewsby'] = 'Expositions par $a';
+
+?>
