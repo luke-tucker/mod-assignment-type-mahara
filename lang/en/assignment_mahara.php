936f6ea299db9e1cdc890a587d2ee3ce088b830b
Aaron Wells <aaronw@catalyst.net.nz>
2011-08-22 15:14:57 +1200

    Mahara "views" are now called "pages"

--- lang/en/assignment_mahara.php
+++ lang/en/assignment_mahara.php
@@ -3,18 +3,18 @@
 
 $string['pluginname'] = 'Mahara portfolio';
 $string['clicktopreview'] = 'click to preview in full-size popup';
-$string['clicktoselect'] = 'click to select view';
+$string['clicktoselect'] = 'click to select page';
 $string['nomaharahostsfound'] = 'No mahara hosts found.';
-$string['noviewscreated'] = 'You have not created any views in {$a}.';
-$string['noviewsfound'] = 'No matching views found in {$a}.';
+$string['noviewscreated'] = 'You have not created any pages in {$a}.';
+$string['noviewsfound'] = 'No matching pages found in {$a}.';
 $string['preview'] = 'Preview';
 $string['site'] = 'Site';
-$string['site_help'] = 'This setting lets you select which Mahara site your students should submit their views from. (The Mahara site must already be configured for mnet networking with this Moodle site.)';
-$string['selectedview'] = 'Submitted View';
-$string['selectmaharaview'] = 'Select one of your {$a->name} portfolio views from this complete list, or <a href="{$a->jumpurl}">click here</a> to visit {$a->name} and create a view right now.';
+$string['site_help'] = 'This setting lets you select which Mahara site your students should submit their pages from. (The Mahara site must already be configured for mnet networking with this Moodle site.)';
+$string['selectedview'] = 'Submitted Page';
+$string['selectmaharaview'] = 'Select one of your {$a->name} portfolio pages from this complete list, or <a href="{$a->jumpurl}">click here</a> to visit {$a->name} and create a page right now.';
 $string['title'] = 'Title';
 $string['typemahara'] = 'Mahara portfolio';
-$string['views'] = 'Views';
-$string['viewsby'] = 'Views by {$a}';
+$string['views'] = 'Pages';
+$string['viewsby'] = 'Pages by {$a}';
 
 ?>
